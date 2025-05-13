86012f81f8c5f7521cf989a420b1549a0bf55a59
root <sushi.honda@gmail.com>
2025-05-13 17:28:03 +0800

    ex00) add: Dog class

---
+++ ex00/Dog.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 17:12:10 by athonda           #+#    #+#             */
+/*   Updated: 2025/05/13 17:26:00 by athonda          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+
+Dog::Dog(const std::string &type) :
+	Animal(type)
+{
+	std::cout << "Dog " << _type << " default constructor called." << std::endl;
+}
+
+Dog::Dog(const Dog &other) :
+	Animal(other)
+{
+	std::cout << "Dog " << _type << " copy constructor called." << std::endl;
+}
+
+Dog	&Dog::operator=(const Dog &other)
+{
+	if (this != &other)
+	{
+		this->_type = other._type;
+		std::cout << "Dog " << _type << " copy asignment operator called." << std::endl;
+	}
+	return (*this);
+}
+
+Dog::~Dog()
+{
+	std::cout << "Dog " << _type << " destructor called." << std::endl;
+}
+
+void	Dog::makeSound()
+{
+	std::cout << "Dog " << _type << " wan wan!! " << std::endl;
+}
--- ex00/Dog.hpp
+++ ex00/Dog.hpp
@@ -1,36 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/13 16:11:39 by athonda           #+#    #+#             */
-/*   Updated: 2025/05/13 17:27:32 by athonda          ###   ########.fr       */
+/*   Created: 2025/05/13 16:41:08 by athonda           #+#    #+#             */
+/*   Updated: 2025/05/13 16:44:35 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 #include "Animal.hpp"
-#include "Dog.hpp"
 
-int	main(void)
+class Dog :
+	public Animal
 {
-	Animal	a("Monster");
-	Animal	b("Titan");
-	Animal	c(a);
-	Animal	d(a);
+public:
+	Dog(const std::string &type);
+	Dog(const Dog &other);
+	Dog	&operator=(const Dog &other);
+	~Dog();
 
-	d = b;
-
-	std::cout << "a: " << a.getType() << std::endl;
-	std::cout << "b: " << b.getType() << std::endl;
-	std::cout << "c: " << c.getType() << std::endl;
-	std::cout << "d: " << d.getType() << std::endl;
-
-	a.makeSound();
-
-	Dog dog("Welsh Corgi");
-	dog.makeSound();
-
-	return (0);
-}
+	void	makeSound();
+private:
+};
