3a51b1d2b7c121744601c3a3412e3eedc6f16718
root <sushi.honda@gmail.com>
2025-05-20 09:39:34 +0800

    ex03) test: Materia & Character in main()
      - b by clone() needs delete, but it will be deleted when character destruct with equipment delete.
      - pure virtual function must be overrided in derived class
      - dont forget put () on function name! this is function!!
    
      still ambiguous in circular include

--- ex03/ICharacter.hpp
+++ ex03/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 18:32:09 by athonda           #+#    #+#             */
-/*   Updated: 2025/05/19 19:21:34 by athonda          ###   ########.fr       */
+/*   Updated: 2025/05/20 09:02:16 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ class ICharacter
 		virtual ~ICharacter();
 		virtual std::string const	&getName() const = 0;
 		virtual void				equip(AMateria *m) = 0;
-		virtual void				unequip(int idx) = 0;
-		virtual void				use(int idx, ICharacter &target) = 0;
+//		virtual void				unequip(int idx) = 0;
+//		virtual void				use(int idx, ICharacter &target) = 0;
 };
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,26 +6,49 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 09:03:50 by athonda           #+#    #+#             */
-/*   Updated: 2025/05/19 09:45:12 by athonda          ###   ########.fr       */
+/*   Updated: 2025/05/20 09:32:34 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AMateria.hpp"
+#include "Ice.hpp"
+#include "ICharacter.hpp"
+#include "Character.hpp"
 
 int	main(void)
 {
-	AMateria	a;
-	AMateria	b;
-	AMateria	c("Kuin");
-	AMateria	d(c);
-	AMateria	e;
+//	AMateria	a;
+//	AMateria	b;
+//	AMateria	c("Kuin");
+//	AMateria	d(c);
+//	AMateria	e;
+//
+////	e = c; //becasue const, no assignmnet
+//
+//	std::cout << "a: type is " << a.getType() << std::endl;
+//	std::cout << "b: type is " << b.getType() << std::endl;
+//	std::cout << "c: type is " << c.getType() << std::endl;
+//	std::cout << "d: type is " << d.getType() << std::endl;
+//	std::cout << "e: type is " << e.getType() << std::endl;
 
-//	e = c; //becasue const, no assignmnet
+	Ice	a("Eleazar no Kama");
+	std::cout << "Ice a: type is " << a.getType() << std::endl;
 
-	std::cout << "a: type is " << a.getType() << std::endl;
-	std::cout << "b: type is " << b.getType() << std::endl;
-	std::cout << "c: type is " << c.getType() << std::endl;
-	std::cout << "d: type is " << d.getType() << std::endl;
-	std::cout << "e: type is " << e.getType() << std::endl;
+	AMateria	*b;
+	b = a.clone();
+	std::cout << "Ice b: type is " << b->getType() << std::endl;
+
+//	AMateria	c;
+//	std::cout << "Materia c: type is " << c.getType() << std::endl;
+
+	Character	d;
+	std::cout << "Character d: name is " << d.getName() << std::endl;
+
+	Character	e("Kirio");
+	std::cout << "Character e: name is " << e.getName() << std::endl;
+
+	e.equip(b);
+	std::cout << "Character e: " << e.getName() << " is equipping b " << b->getType() << std::endl;
+
 	return (0);
 }
