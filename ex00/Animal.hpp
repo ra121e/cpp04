c27b637a4d3c84513692d5310ca3af3f42105a1e
root <sushi.honda@gmail.com>
2025-05-21 20:34:32 +0800

    ex00-01) refactor: derived class call Brain class func
      What I wanted to do is set and get array elements in Brain.
      So, made 2 functions in derived class Cat and Dog
    
      But, polymorphism does not accept the derived class original function.
      Polymorphism can use Base class functions and overrided ones.
    
      So, put 2 functions in Animal class

--- ex00/Animal.hpp
+++ ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 15:56:09 by athonda           #+#    #+#             */
-/*   Updated: 2025/05/20 20:32:33 by athonda          ###   ########.fr       */
+/*   Updated: 2025/05/21 20:15:39 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,11 @@ public:
 	Animal	&operator=(const Animal &other);
 	virtual ~Animal();
 
-	virtual void	makeSound() const;
-	std::string		getType(void) const;
+	virtual void		makeSound() const;
+	std::string			getType(void) const;
+
+	virtual void		setIdeas(int i, std::string const &idea);
+	virtual std::string	const &getIdeas(int i) const;
 
 protected:
 	std::string	_type;
