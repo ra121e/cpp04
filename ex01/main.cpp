1a67e8d2eb641334e129d7f75d801adc6cf51646
root <sushi.honda@gmail.com>
2025-05-22 13:40:11 +0800

    ex01) tests: add const base pointer and non-const
      derived instance as Dog and Cat should be non-const for setting brains.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 11:15:38 by athonda           #+#    #+#             */
-/*   Updated: 2025/05/21 23:19:57 by athonda          ###   ########.fr       */
+/*   Updated: 2025/05/22 13:39:59 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,22 +89,34 @@ int	main(void)
 		std::cout << "Cat: f ideas[" << i << "] is " << f.getIdeas(i) << std::endl;
 	}
 
+	std::cout << "===== test mandatory case by newed variables =====" << std::endl;
+	const Animal *j = new Dog();
+	const Animal *i = new Cat();
+
+	j->makeSound();
+	i->makeSound();
+
+	delete (j);
+	delete (i);
+
 	std::cout << "===== test Animal array by auto variables" << std::endl;
 	Dog		akita;
-	akita.getBrain()->setIdeas(0, "Cute is best");
+	akita.getBrain()->setIdeas(0, "I am akita");
 
-	Cat		carico;
-	carico.getBrain()->setIdeas(0, "sleepy");
+	Cat		calico;
+	calico.getBrain()->setIdeas(0, "calico always sleepy");
 
 	Cat		mike;
-	mike.getBrain()->setIdeas(0, "hungry");
+	mike.getBrain()->setIdeas(0, "mike is hungry");
 
 	Dog		cogi;
-	cogi.getBrain()->setIdeas(0, "hunting!!");
+	cogi.getBrain()->setIdeas(0, "cogi loves hunting!!");
+
+	// wrap with animal pointer
 	Animal	*animal[4];
 
 	animal[0] = &akita;
-	animal[1] = &carico;
+	animal[1] = &calico;
 	animal[2] = &mike;
 	animal[3] = &cogi;
 
@@ -113,6 +125,37 @@ int	main(void)
 	{
 		animal[i]->makeSound();
 		std::cout << animal[i]->getType() << " has an idea as " << animal[i]->getIdeas(0) << std::endl;
+	}
+	std::cout << "sounds correct!!." << std::endl << std::endl;
+
+
+	std::cout << "===== test const for Base pointer and test deep copy =====" << std::endl;
+
+	Cat	abyssinian(mike);
+	Cat	siamese(mike);
+	Cat	persian(calico);
+
+	siamese = persian;
+
+	Dog	husky(akita);
+	Dog	pug(akita);
+	Dog	doberman(cogi);
+
+	pug = doberman;
+
+	Animal const	*pets[4] =
+	{
+		&husky,
+		&pug,
+		&abyssinian,
+		&siamese
+	};
+
+	std::cout << std::endl << "Here are the voices of the animals." << std::endl;
+	for(int i = 0; i < 4; ++i)
+	{
+		pets[i]->makeSound();
+		std::cout << pets[i]->getType() << " has an idea as " << pets[i]->getIdeas(0) << std::endl;
 	}
 	std::cout << "sounds correct!!." << std::endl << std::endl;
 
