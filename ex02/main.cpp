b33b95fffdf70b44aa3aecaecf64f45ee877637a
root <sushi.honda@gmail.com>
2025-05-23 10:57:32 +0800

    ex02) doc: add comment of declaration sample

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 23:03:47 by athonda           #+#    #+#             */
-/*   Updated: 2025/05/22 17:21:32 by athonda          ###   ########.fr       */
+/*   Updated: 2025/05/23 10:56:26 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,83 +21,31 @@ void printBanner(const std::string& title) {
 
 int main() {
 	std::cout << "===== test Animal class instance =====" << std::endl;
-	//Animal testAnimal; // This line should cause a compilation error because Animal is abstract.
-	// std::cout << "If you see this, Animal was instantiated (which is wrong)." << std::endl;
-	// To test this, you would uncomment the line above and try to compile.
-	// For a runnable program, we keep it commented.
+	//Animal testAnimal;
+	std::cout << "Animal testAnimal;" << std::endl;
 	std::cout << "(The line 'Animal testAnimal;' is commented out to allow compilation.)" << std::endl;
 	std::cout << "(To verify, uncomment it and compilation should fail.)" << std::endl;
 
 
-    printBanner("Polymorphic Array of Animals (Dogs and Cats)");
-    const int num_animals = 4; // Reduced for brevity, can be any even number
-    Animal* animals[num_animals];
-
-    std::cout << "\nCreating Dogs for the first half:" << std::endl;
-    for (int k = 0; k < num_animals / 2; ++k) {
-        animals[k] = new Dog();
-    }
-    std::cout << "\nCreating Cats for the second half:" << std::endl;
-    for (int k = num_animals / 2; k < num_animals; ++k) {
-        animals[k] = new Cat();
-    }
-
-    std::cout << "\nAnimals making sounds and showing types:" << std::endl;
-    for (int k = 0; k < num_animals; ++k) {
-        std::cout << "Type: " << animals[k]->getType() << " -> ";
-        animals[k]->makeSound(); // Polymorphic call
-    }
-
-//    std::cout << "\nTesting Dog's ideas via Animal pointer:" << std::endl;
-//    // To access Dog-specific methods like setIdea/getIdea through Animal*,
-//    // you would need to downcast, which is generally not good practice without type checking.
-//    // However, for this test, if we are sure of the type:
-//    if (num_animals > 0 && animals[0]->getType() == "Dog") {
-//        Dog* firstDog = static_cast<Dog*>(animals[0]); // Use dynamic_cast in more complex scenarios
-//        firstDog->setIdea(0, "A brilliant idea from a Dog!");
-//        std::cout << "Dog's Idea[0]: " << firstDog->getIdea(0) << std::endl;
-//    }
-//
-//
-//    std::cout << "\nDeleting all animals (destructors should be called in order):" << std::endl;
-//    for (int k = 0; k < num_animals; ++k) {
-//        delete animals[k];
-//        animals[k] = NULL;
-//    }
-//
-//    printBanner("Deep Copy Test (Dog) - OCF Check");
-//    std::cout << "\nCreating originalDog:" << std::endl;
-//    Dog originalDog;
-//    originalDog.setIdea(0, "Original Dog Idea");
-//    originalDog.setIdea(1, "Another Original Dog Idea");
-//    std::cout << "OriginalDog Idea[0]: " << originalDog.getIdea(0) << std::endl;
-//
-//    std::cout << "\nCreating copiedDog from originalDog (Copy Constructor):" << std::endl;
-//    Dog copiedDog = originalDog;
-//    std::cout << "CopiedDog Idea[0] (should be same as original): " << copiedDog.getIdea(0) << std::endl;
-//
-//    std::cout << "\nModifying originalDog's Idea[0]:" << std::endl;
-//    originalDog.setIdea(0, "MODIFIED Original Dog Idea");
-//    std::cout << "OriginalDog Idea[0] (now modified): " << originalDog.getIdea(0) << std::endl;
-//    std::cout << "CopiedDog Idea[0] (should be UNCHANGED): " << copiedDog.getIdea(0) << std::endl;
-//
-//
-//    std::cout << "\nCreating assignedDog and assigning originalDog to it (Assignment Operator):" << std::endl;
-//    Dog assignedDog; // Default construction
-//    std::cout << "AssignedDog Idea[0] (before assignment): " << assignedDog.getIdea(0) << std::endl; // Shows default brain idea
-//    assignedDog = originalDog; // Assignment
-//    std::cout << "AssignedDog Idea[0] (after assignment, should match originalDog's modified idea): " << assignedDog.getIdea(0) << std::endl;
-//
-//    std::cout << "\nModifying originalDog's Idea[0] again:" << std::endl;
-//    originalDog.setIdea(0, "SUPER MODIFIED Original Dog Idea");
-//    std::cout << "OriginalDog Idea[0] (modified again): " << originalDog.getIdea(0) << std::endl;
-//    std::cout << "CopiedDog Idea[0] (still UNCHANGED from its copy time): " << copiedDog.getIdea(0) << std::endl;
-//    std::cout << "AssignedDog Idea[0] (should be 'MODIFIED Original Dog Idea'): " << assignedDog.getIdea(0) << std::endl;
-//
-//    // Similar test for Cat can be added.
+	std::cout << "===== test same to ex01  =====" << std::endl;
+	const int num_animals = 4; // Reduced for brevity, can be any even number
+	Animal* animals[num_animals];
+	std::cout << "\nCreating Dogs for the first half:" << std::endl;
+	for (int k = 0; k < num_animals / 2; ++k) {
+		animals[k] = new Dog();
+	}
+	std::cout << "\nCreating Cats for the second half:" << std::endl;
+	for (int k = num_animals / 2; k < num_animals; ++k) {
+		animals[k] = new Cat();
+	}
+	std::cout << "\nAnimals making sounds and showing types:" << std::endl;
+	for (int k = 0; k < num_animals; ++k) {
+		std::cout << "Type: " << animals[k]->getType() << " -> ";
+		animals[k]->makeSound(); // Polymorphic call
+	}
 
 	{
-		std::cout << "Entering new scope..." << std::endl;
+		std::cout << "\nEntering new scope..." << std::endl;
 		Dog stackDog;
 		Cat stackCat;
 		stackDog.makeSound();
@@ -106,7 +54,7 @@ int main() {
 	}
 
 
-    printBanner("All tests finished.");
-    // Destructors for originalDog, copiedDog, assignedDog (and their Brains) will be called here.
-    return 0;
+	printBanner("All tests finished.");
+	// Destructors for originalDog, copiedDog, assignedDog (and their Brains) will be called here.
+	return 0;
 }
