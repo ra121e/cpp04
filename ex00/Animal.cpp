fda0dd8ef2aca2fbad86ed68184ac0797b799398
root <sushi.honda@gmail.com>
2025-05-23 09:38:55 +0800

    ex00) doc: misspelling dummy

--- ex00/Animal.cpp
+++ ex00/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:03:52 by athonda           #+#    #+#             */
-/*   Updated: 2025/05/21 20:15:48 by athonda          ###   ########.fr       */
+/*   Updated: 2025/05/23 09:37:45 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,12 +57,12 @@ std::string	Animal::getType(void) const
 
 void	Animal::setIdeas(int i, std::string const &idea)
 {
-	std::cout << "this is dammy function. index is " << i << " idea is " << idea << std::endl;
+	std::cout << "this is dummy function. index is " << i << " idea is " << idea << std::endl;
 }
 
 std::string	const &Animal::getIdeas(int i) const
 {
-	std::cout << "this is dammy function. index is " << i << std::endl;
-	static std::string dammy = "this is dammy";
-	return (dammy);
+	std::cout << "this is dummy function. index is " << i << std::endl;
+	static std::string dummy = "this is dummy";
+	return (dummy);
 }
